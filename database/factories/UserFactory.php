340ec9148441dd7a39691def1f2d278dabb78bdb
Boris <bobabonanadz@mail.ru>
2021-12-17 17:40:33 +0300

    Changed factory & migration User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,23 +2,33 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
     /**
-     * Define the model's default state.
-     *
      * @return array
+     * @throws Exception
      */
-    public function definition()
+    public function definition(): array
     {
+        $active = $this->faker->boolean;
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'first_name'     => $this->faker->firstName(),
+            'last_name'      => $this->faker->lastName(),
+            'email'          => $this->faker->unique()->safeEmail(),
+            'phone'          => random_int(9000000000, 9999999999),
+            'status'         => $active ? User::STATUS_ACTIVE : User::STATUS_PENDING,
+            'password'       => 'password', // Hashed in User class set()
+            'full_address'   => [
+                'street'   => $this->faker->streetAddress(),
+                'city'     => $this->faker->city(),
+                'country'  => $this->faker->country(),
+                'zip_code' => $this->faker->postcode(),
+            ],
             'remember_token' => Str::random(10),
         ];
     }
