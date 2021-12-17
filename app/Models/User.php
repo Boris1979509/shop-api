e5b7f49230c0e216233664f65653d378c6a23927
Boris <bobabonanadz@mail.ru>
2021-12-17 17:41:27 +0300

    Changed model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,21 +6,35 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App\Models
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    // statuses
+    public const STATUS_ACTIVE = 'active';
+    public const STATUS_PENDING = 'pending';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
+    protected array $fillable = [
+        'first_name',
+        'last_name',
         'email',
+        'phone',
         'password',
+        'status',
+        'full_address',
     ];
 
     /**
@@ -28,7 +42,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = [
+    protected array $hidden = [
         'password',
         'remember_token',
     ];
@@ -38,7 +52,33 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
+    protected array $casts = [
         'email_verified_at' => 'datetime',
+        'full_address'      => 'object',
+        'phone'             => 'integer',
     ];
+
+    /**
+     * @param $password
+     */
+    public function setPasswordAttribute($password): void
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
+
+    /**
+     * @param $first_name
+     */
+    public function setFirstNameAttribute($first_name): void
+    {
+        $this->attributes['first_name'] = Str::ucfirst($first_name);
+    }
+
+    /**
+     * @param $last_name
+     */
+    public function setLastNameAttribute($last_name): void
+    {
+        $this->attributes['last_name'] = Str::ucfirst($last_name);
+    }
 }
